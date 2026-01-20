f6ae7b5bba997a597e2143fb167d1f175390ff5a
GabrielHTissi <gabriel.hoeft.tissi@icloud.com>
2026-01-20 19:59:09 -0300

    initial commit: estrutura básica do conversor de medidas

---
+++ main.cpp
@@ -0,0 +1,20 @@
+///meu primeiro projeto em C++, pretendo aprender o basico da linguagem e implentar 
+//alguns algoritmos simples com funcionalidades que ajudem no meu trabalho atual de PCP.
+
+
+#include <iostream>//controle de entra e saida
+#include <iomanip>//formatação de numeros
+#include <string>//manipulação de strings
+
+
+int main() { 
+
+double medidaMM, medidaPG; //ocupa o dobro de memória, mas é muito mais preciso que o float
+const float CONVERSAO = 25.4;//constante de conversão de polegadas para milímetros(1 polegada = 25,4 mm)
+
+std::cout <<"DIGITE UMA MEDIDA EM MILÍMETROS: ";//solicita ao usuário que digite uma medida em milímetros
+std::cin >> medidaMM;//lê a medida em milímetros fornecida pelo usuárioS
+std::setprecision(4);//define a precisão dos números exibidos
+    return 0;
+}
+s
