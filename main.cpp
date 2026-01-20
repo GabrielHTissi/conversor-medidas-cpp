e0a3cbe570fb9367f1d9d7c7b05f5017ef0bcf0f
GabrielHTissi <gabriel.hoeft.tissi@icloud.com>
2026-01-20 20:48:38 -0300

    Funcionalidade: Implementa conversão de mm para pol com precisão de 4 casas

--- main.cpp
+++ main.cpp
@@ -15,6 +15,13 @@ const float CONVERSAO = 25.4;//constante de conversão de polegadas para milíme
 std::cout <<"DIGITE UMA MEDIDA EM MILÍMETROS: ";//solicita ao usuário que digite uma medida em milímetros
 std::cin >> medidaMM;//lê a medida em milímetros fornecida pelo usuárioS
 std::setprecision(4);//define a precisão dos números exibidos
-    return 0;
+
+medidaPG=medidaMM/CONVERSAO;//converte a medida de milímetros para polegadas
+std::cout << "O valor em polegadas é: " << std::fixed << std::setprecision(4) << medidaPG << " pol" << std::endl;//exibe o valor convertido em polegadas com 4 casas decimais
+
+
+
+
+
+return 0;
 }
-s
